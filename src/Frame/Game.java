b99ba360c288cb3f0c28fbeb8be8503cc3f799be
david-schor <david.schor2000@gmail.com>
2016-04-29 11:51:47 +0200

    First Version of Bouny Ball

---
+++ src/Frame/Game.java
@@ -0,0 +1,65 @@
+package Frame;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+	
+	@SuppressWarnings("serial")
+	public class Game extends JPanel{
+		int x = 0;
+		int y = 0;
+		int durchmesser = 50;
+		int speedX = 3;
+		int speedY = 1;
+		public static int h;
+		public static int w;
+		
+		public static void main(String[] args) throws InterruptedException {
+			JFrame frame = new JFrame("Sample Frame");
+			Game spiel = new Game();
+			frame.add(spiel);
+			frame.pack();
+			frame.setVisible(true);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
+			while(true){
+				spiel.moveBall();
+				spiel.repaint();
+				h = (int) frame.getContentPane().getSize().getHeight();
+				w = (int) frame.getContentPane().getSize().getWidth();
+				spiel.BallCollision();
+				Thread.sleep(10);
+			}
+		}
+		
+		private void moveBall(){
+			x = x + speedX;
+			y = y + speedY;
+		}
+		
+		public void BallCollision(){
+			if(x + durchmesser > w){
+				speedX = speedX > 0 ? -speedX : speedX;
+			}
+			if(x < 0){
+				speedX = speedX < 0 ? -speedX : speedX;
+			}
+			if(y + durchmesser > h){
+				speedY = speedY > 0 ? -speedY : speedY;
+			}
+			if(y < 0){
+				speedY = speedY < 0 ? -speedY : speedY;
+			}
+		}
+		
+		@Override
+		public void paint(Graphics g){
+			super.paint(g);
+			Graphics2D g2d = (Graphics2D) g;
+			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+			g2d.fillOval(x, y, durchmesser, durchmesser);
+		}
+
+}
