70d560a7540074cbd734c86096483f2365518437
David Schor <david.schor2000@gmail.com>
2017-05-31 21:05:42 +0200

    Refactor Game

--- src/Frame/Game.java
+++ src/Frame/Game.java
@@ -1,4 +1,4 @@
-package Frame;
+package frame;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -10,30 +10,31 @@ import javax.swing.JPanel;
 	public class Game extends JPanel{
 		
 		//Initzial Variables
-		int x = 0;
-		int y = 0;
-		int durchmesser = 50;
-		int speedX = 3;
-		int speedY = 1;
+		public int x = 0;
+		public int y = 0;
+		public int diameter = 50;
+		public int speedX = 3;
+		public int speedY = 1;
 		public static int h;
 		public static int w;
 		
 		public static void main(String[] args) throws InterruptedException {
 			
 			//Create Frame
-			JFrame frame = new JFrame("Sample Frame");
-			Game spiel = new Game();
-			frame.add(spiel);
+			JFrame frame = new JFrame("Game Frame");
+			Game game = new Game();
+			frame.add(game);
 			frame.pack();
 			frame.setVisible(true);
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			
+			//Do Loop to call Methodes
 			while(true){
-				spiel.moveBall();
-				spiel.repaint();
+				game.moveBall();
+				game.repaint();
 				h = (int) frame.getContentPane().getSize().getHeight();
 				w = (int) frame.getContentPane().getSize().getWidth();
-				spiel.BallCollision();
+				game.ballCollision();
 				Thread.sleep(10);
 			}
 		}
@@ -44,29 +45,29 @@ import javax.swing.JPanel;
 			y = y + speedY;
 		}
 		
-		//Collision with wall
-		public void BallCollision(){
-			if(x + durchmesser > w){
+		//Collision with Wall
+		public void ballCollision(){
+			if(x + diameter > w)
 				speedX = speedX > 0 ? -speedX : speedX;
-			}
-			if(x < 0){
+			
+			if(x < 0)
 				speedX = speedX < 0 ? -speedX : speedX;
-			}
-			if(y + durchmesser > h){
+			
+			if(y + diameter > h)
 				speedY = speedY > 0 ? -speedY : speedY;
-			}
-			if(y < 0){
+			
+			if(y < 0)
 				speedY = speedY < 0 ? -speedY : speedY;
-			}
+			
 		}
 		
 		//Draw Ball
 		@Override
 		public void paint(Graphics g){
 			super.paint(g);
-			Graphics2D g2d = (Graphics2D) g;
-			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-			g2d.fillOval(x, y, durchmesser, durchmesser);
+			Graphics2D ball = (Graphics2D) g;
+			ball.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+			ball.fillOval(x, y, durchmesser, durchmesser);
 		}
 
 }
